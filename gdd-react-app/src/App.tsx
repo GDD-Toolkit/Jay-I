4513278aa9dd69684bb0a3f56b6fd8e76ade5a3a
Tobi <adegbajuoezekiel@gmail.com>
2023-10-25 20:32:22 -0400

    Added the input fields with the submit btn

--- gdd-react-app/src/App.tsx
+++ gdd-react-app/src/App.tsx
@@ -1,22 +1,60 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        {/* First Name */}
+        <div className="input-div">
+          <input
+            className="input-field"
+            type="text"
+            name="First Name"
+            id="firstName"
+            placeholder="Enter your first name"
+          />
+        </div>
+
+        {/* Last Name */}
+        <div className="input-div">
+          <input
+            className="input-field"
+            type="text"
+            name="Last Name"
+            id="lastName"
+            placeholder="Enter your last name"
+          />
+        </div>
+
+        {/* User Role Dropdown */}
+        <div className="input-div">
+          <select className="input-field" name="User Role" id="userRole">
+            <option value="" disabled selected hidden>
+              Select your role
+            </option>
+            <option value="project manager">Project Manager</option>
+            <option value="student">Student</option>
+            <option value="professor">Professor</option>
+          </select>
+        </div>
+
+        {/* Affiliation */}
+        <div className="input-div">
+          <input
+            className="input-field"
+            type="text"
+            name="Affiliation"
+            id="affiliation"
+            placeholder="Enter your affiliation"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
---
+++ gdd-react-app/src/types/ProjectDetails.tsx
@@ -0,0 +1,40 @@
+export interface UserInput {
+  user: string;
+  projectAbstract: string;
+  projectRegion: string;
+  projectTitle: string;
+  projectDetails: ProjectDetails;
+}
+
+export interface ProjectDetails {
+  ethical: Ethicality;
+  desire: Desirability;
+  eco: string;
+  feasibility: Feasbility;
+  viable: Viability;
+}
+
+export interface Ethicality {
+  problem: string;
+  ethicalDeliberation: string;
+  societalImpact: string;
+}
+
+export interface Desirability {
+  opportunity: string;
+  stakeholders: string;
+  valueProposition: string;
+  outcomes: string;
+}
+
+export interface Feasbility {
+  activities: string;
+  partners: string;
+  channels: string;
+}
+
+export interface Viability {
+  resources: string;
+  revenue: string;
+  cost: string;
+}

d92e5f222630e917c755254b1e7ca8d8a38f8c6a
Jessica Nguyen <nguyen15@terpmail.umd.edu>
2023-11-07 01:38:54 -0500

    add basic non-firebase functionality to inputs.

--- gdd-react-app/src/App.tsx
+++ gdd-react-app/src/App.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [role, setRole] = useState("");
+  const [affiliation, setAffiliation] = useState("");
+
+  const handleChanges = (event) => {
+    event.preventDefault();
+    console.log(firstName);
+    console.log(lastName);
+    console.log(role);
+    console.log(affiliation);
+  };
+
   return (
     <div className="App">
       <form className="form">
         {/* First Name */}
         <div className="input-div">
           <input
+            onChange={e => setFirstName(e.target.value)}
             className="input-field"
             type="text"
             name="First Name"
@@ -19,6 +33,7 @@ function App() {
         {/* Last Name */}
         <div className="input-div">
           <input
+            onChange={e => setLastName(e.target.value)}
             className="input-field"
             type="text"
             name="Last Name"
@@ -29,8 +44,14 @@ function App() {
 
         {/* User Role Dropdown */}
         <div className="input-div">
-          <select className="input-field" name="User Role" id="userRole">
-            <option value="" disabled selected hidden>
+        <select
+            className="input-field"
+            name="User Role"
+            id="userRole"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="" disabled hidden>
               Select your role
             </option>
             <option value="project manager">Project Manager</option>
@@ -42,6 +63,7 @@ function App() {
         {/* Affiliation */}
         <div className="input-div">
           <input
+            onChange={e => setAffiliation(e.target.value)}
             className="input-field"
             type="text"
             name="Affiliation"
@@ -51,7 +73,7 @@ function App() {
         </div>
 
         {/* Submit Button */}
-        <button className="submit-btn" type="submit">
+        <button className="submit-btn" type="submit" onClick={handleChanges}>
           Submit
         </button>
       </form>

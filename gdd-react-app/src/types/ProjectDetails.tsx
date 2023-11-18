2880bb6782a93b5ca6490e8b2a84870d1c8ded4d
Jessica Nguyen <nguyen15@terpmail.umd.edu>
2023-11-18 18:29:02 -0500

    edited type properties and installed bootstrap

--- gdd-react-app/src/types/ProjectDetails.tsx
+++ gdd-react-app/src/types/ProjectDetails.tsx
@@ -1,15 +1,13 @@
-export interface UserInput {
-  user: string;
+import { ToolkitUser } from "./ToolkitUser";
+
+export interface ProjectDetails {
+  user: ToolkitUser;
   projectAbstract: string;
   projectRegion: string;
   projectTitle: string;
-  projectDetails: ProjectDetails;
-}
-
-export interface ProjectDetails {
-  ethical: Ethicality;
-  desire: Desirability;
-  eco: string;
+  ethicality: Ethicality;
+  desirability: Desirability;
+  ecosystem: string;
   feasibility: Feasbility;
   viable: Viability;
 }
@@ -38,43 +36,3 @@ export interface Viability {
   revenue: string;
   cost: string;
 }
-=======
-    user: string;
-    projectAbstract: string;
-    projectRegion: string;
-    projectTitle: string;
-    projectDetails: ProjectDetails;
-  }
-
-  export interface ProjectDetails {
-    ethical: Ethicality;
-    desire: Desirability;
-    eco: string;
-    feasibility: Feasbility;
-    viable: Viability;
-  }
-
-  export interface Ethicality {
-    problem: string;
-    ethicalDeliberation: string;
-    societalImpact: string;
-  }
-
-  export interface Desirability {
-    opportunity: string;
-    stakeholders: string;
-    valueProposition: string;
-    outcomes: string;
-  }
-
-  export interface Feasbility {
-    activities: string;
-    partners: string;
-    channels: string;
-  }
-
-  export interface Viability {
-    resources: string;
-    revenue: string;
-    cost: string;
-  }

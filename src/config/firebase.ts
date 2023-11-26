b2835566d339a63631a4c0680e4377d5f2f3b26e
Jessica Nguyen <nguyen15@terpmail.umd.edu>
2023-11-26 02:59:37 -0500

    removed firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,30 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { getAnalytics } from 'firebase/analytics';
-import { getFunctions } from 'firebase/functions';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getFunctions } from 'firebase/functions';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBE5uCMEp2u3G2qBvkK9oGOrPVWUfjcrcI",
-  authDomain: "gdd-toolkit.firebaseapp.com",
-  databaseURL: "https://gdd-toolkit-default-rtdb.firebaseio.com",
-  projectId: "gdd-toolkit",
-  storageBucket: "gdd-toolkit.appspot.com",
-  messagingSenderId: "792191653355",
-  appId: "1:792191653355:web:91c13f8574770df34bb5ae",
-  measurementId: "G-LHC1PWRS7B"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBE5uCMEp2u3G2qBvkK9oGOrPVWUfjcrcI",
+//   authDomain: "gdd-toolkit.firebaseapp.com",
+//   databaseURL: "https://gdd-toolkit-default-rtdb.firebaseio.com",
+//   projectId: "gdd-toolkit",
+//   storageBucket: "gdd-toolkit.appspot.com",
+//   messagingSenderId: "792191653355",
+//   appId: "1:792191653355:web:91c13f8574770df34bb5ae",
+//   measurementId: "G-LHC1PWRS7B"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const functions = getFunctions(app, 'us-east4');
-export default app;
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+// export const functions = getFunctions(app, 'us-east4');
+// export default app;

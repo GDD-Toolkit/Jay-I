dd89c9cb8f3d4de5f1e5e5c98eab2df124a8b13f
Jessica Nguyen <nguyen15@terpmail.umd.edu>
2023-11-26 03:25:36 -0500

    set up firebase from scratch

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,30 +1,22 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
-// import { getAnalytics } from 'firebase/analytics';
-// import { getFunctions } from 'firebase/functions';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBE5uCMEp2u3G2qBvkK9oGOrPVWUfjcrcI",
-//   authDomain: "gdd-toolkit.firebaseapp.com",
-//   databaseURL: "https://gdd-toolkit-default-rtdb.firebaseio.com",
-//   projectId: "gdd-toolkit",
-//   storageBucket: "gdd-toolkit.appspot.com",
-//   messagingSenderId: "792191653355",
-//   appId: "1:792191653355:web:91c13f8574770df34bb5ae",
-//   measurementId: "G-LHC1PWRS7B"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBE5uCMEp2u3G2qBvkK9oGOrPVWUfjcrcI",
+  authDomain: "gdd-toolkit.firebaseapp.com",
+  databaseURL: "https://gdd-toolkit-default-rtdb.firebaseio.com",
+  projectId: "gdd-toolkit",
+  storageBucket: "gdd-toolkit.appspot.com",
+  messagingSenderId: "792191653355",
+  appId: "1:792191653355:web:9c3cbd9e603ffcb24bb5ae",
+  measurementId: "G-RP1HGGTFQ5"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
-// export const functions = getFunctions(app, 'us-east4');
-// export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

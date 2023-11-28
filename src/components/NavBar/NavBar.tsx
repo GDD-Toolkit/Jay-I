b7b95fdf433b2c6751491e6ede213fcaf61634c8
Your Name <jnguyengg@gmail.com>
2023-11-28 01:05:04 -0500

    Added NavBar placeholder

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,18 @@
+/*
+This is an example of what the NavBar code should look like. When importing the NavBar to any page, one would have to include the
+the title property so that it can be displayed in a way demonstrated below:
+
 import React from "react";
 
-const NavBar = (): JSX.Element => {
+interface props {
+    title: String;
+}
+
+const NavBar = ({title}: props): React.ReactElement => {
     return (
-        <div>Hi</div>
+        <div>{title}</div>
     );
 }
 
 export default NavBar;
+*/

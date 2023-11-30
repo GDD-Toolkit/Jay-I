379fca2a3576c347bf1d2d0fa6721a73f1cbbdbc
Your Name <jnguyengg@gmail.com>
2023-11-30 12:41:42 -0500

    added color vars and muitheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from "./index.module.css";
 import React, { useState } from "react";
+import {theme} from './mui.ts';
+import { ThemeProvider } from "@mui/material";
 
 function App() {
   const [firstName, setFirstName] = useState("");
@@ -18,69 +20,71 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {/* <NavBar title="This is where the NavBar should go" /> */}
-      <form className={styles.form}>
-        {/* First Name */}
-        <div className={styles.inputDiv}>
-          <input
-            onChange={e => setFirstName(e.target.value)}
-            className={styles.inputField}
-            type="text"
-            name="First Name"
-            id="firstName"
-            placeholder="Enter your first name"
-          />
-        </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {/* <NavBar title="This is where the NavBar should go" /> */}
+        <form className={styles.form}>
+          {/* First Name */}
+          <div className={styles.inputDiv}>
+            <input
+              onChange={e => setFirstName(e.target.value)}
+              className={styles.inputField}
+              type="text"
+              name="First Name"
+              id="firstName"
+              placeholder="Enter your first name"
+            />
+          </div>
 
-        {/* Last Name */}
-        <div className={styles.inputDiv}>
-          <input
-            onChange={e => setLastName(e.target.value)}
-            className={styles.inputField}
-            type="text"
-            name="Last Name"
-            id="lastName"
-            placeholder="Enter your last name"
-          />
-        </div>
+          {/* Last Name */}
+         <div className={styles.inputDiv}>
+            <input
+              onChange={e => setLastName(e.target.value)}
+              className={styles.inputField}
+              type="text"
+              name="Last Name"
+              id="lastName"
+              placeholder="Enter your last name"
+            />
+          </div>
 
-        {/* User Role Dropdown */}
-        <div className={styles.inputDiv}>
-        <select
-            className={styles.inputField}
-            name="User Role"
-            id="userRole"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-          >
-            <option value="" disabled hidden>
-              Select your role
-            </option>
-            <option value="project manager">Project Manager</option>
-            <option value="student">Student</option>
-            <option value="professor">Professor</option>
-          </select>
-        </div>
+          {/* User Role Dropdown */}
+          <div className={styles.inputDiv}>
+          <select
+              className={styles.inputField}
+              name="User Role"
+              id="userRole"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value="" disabled hidden>
+                Select your role
+              </option>
+              <option value="project manager">Project Manager</option>
+              <option value="student">Student</option>
+              <option value="professor">Professor</option>
+            </select>
+          </div>
 
-        {/* Affiliation */}
-        <div className={styles.inputDiv}>
-          <input
-            onChange={e => setAffiliation(e.target.value)}
-            className={styles.inputField}
-            type="text"
-            name="Affiliation"
-            id="affiliation"
-            placeholder="Enter your affiliation"
-          />
-        </div>
+          {/* Affiliation */}
+          <div className={styles.inputDiv}>
+            <input
+              onChange={e => setAffiliation(e.target.value)}
+              className={styles.inputField}
+              type="text"
+              name="Affiliation"
+              id="affiliation"
+              placeholder="Enter your affiliation"
+            />
+         </div>
 
-        {/* Submit Button */}
-        <button className={styles.submitBtn} type="submit" onClick={handleChanges}>
-          Submit
-        </button>
-      </form>
-    </div>
+          {/* Submit Button */}
+          <button className={styles.submitBtn} type="submit" onClick={handleChanges}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </ThemeProvider>
   );
 }
 

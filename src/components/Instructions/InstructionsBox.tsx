b634c0b4eb8faa7801c0f06d4f2105bc36f0edbe
ojieok <142185852+ojieok@users.noreply.github.com>
2023-11-30 19:33:18 -0500

    Update InstructionsBox.tsx

--- src/components/Instructions/InstructionsBox.tsx
+++ src/components/Instructions/InstructionsBox.tsx
@@ -1,1 +1,11 @@
-'Instructions: Click on each box to type and enter the appropriate information inside them. Click (?) to get more specific questions. Press “next” to continue and to save your work.'
+import React from 'react';
+
+const instructionsBox = () => {
+    return(
+        <div className = 'instruction-box'>
+            <p> <b>Instructions:</b> Click on each box to type and enter the appropriate information inside them. Click (?) to get more specific questions. Press “next” to continue and to save your work.
+</p>
+        </div>
+    );
+};
+

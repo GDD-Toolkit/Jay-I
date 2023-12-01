9bb22a04b5fa2993da5bcd432dc74b18f8aa9e76
Your Name <jnguyengg@gmail.com>
2023-12-01 01:05:30 -0500

    added instructions box styles + implemented box

--- src/components/Instructions/InstructionsBox.tsx
+++ src/components/Instructions/InstructionsBox.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './InustructionsBox.module.css';
 
-const instructionsBox = () => {
+const InstructionsBox = () => {
     return(
-        <div className = 'instruction-box'>
-            <p> <b>Instructions:</b> Click on each box to type and enter the appropriate information inside them. Click (?) to get more specific questions. Press “next” to continue and to save your work.
-</p>
+        <div className={styles.instructionBox}>
+            <p> 
+                <b>Instructions:</b> Click on each box to type and enter the appropriate information inside them. 
+                Click (<b>?</b>) to get more specific questions. Press “next” to continue and to save your work.
+            </p>
         </div>
     );
 };
 
+export default InstructionsBox;

31a250b1c0f51287c132df11b306d1c5f1a50cd2
Your Name <jnguyengg@gmail.com>
2023-12-21 03:24:00 -0500

    fixed logo responsiveness to zoom

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,9 +9,9 @@ const NavBar = ({ title }: props): React.ReactElement => {
   const logo = require('../../assets/GDDLogo.png');
   return (
     <div className={styles.navBar}>
-      <div className={styles.logoHolder}>
+     
         <img src={logo} className={styles.logo}/>
-      </div>
+     
       <div className={styles.holder}>
         <h1 className={styles.title}>{title}</h1>
       </div>
